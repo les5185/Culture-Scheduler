e8a9a1c0b14ba7be2bb3c71eae390c27b2edc2ce
Tiffany Lee <tiffany@Tiffanyui-MacBook-Pro.local>
2021-05-15 03:08:27 +0900

    basic models

--- CultureServer/content/models.py
+++ CultureServer/content/models.py
@@ -1,5 +1,32 @@
 from django.db import models
 
 # Create your models here.
-class Content(models):
-  title = models.CharField()
+class Content(models.Model):
+    rating_choice = (
+      ('추천', '추천'),
+      ('비추천', '비추천'),
+    )
+    title = models.CharField(max_length=30)
+    genre = models.CharField(max_length=10)
+    cast = models.CharField(max_length=30)
+    director = models.CharField(max_length=10)
+    runnning_time = models.IntegerField(help_text='모든 러닝 타임은 분으로 표시됩니다')
+    location = models.CharField(max_length=10)
+    plot = models.CharField(max_length=200)
+    image = models.ImageField(null=True, blank=True, upload_to='images')
+
+#위에랑 아래 확인 
+class Photo(models.Model):
+    content_photo = models.ForeignKey('Content', on_delete=models.CASCADE, related_name='photos', verbose_name='대표 이미지')
+    photo_file = models.ImageField('사진', upload_to='photo/%Y/%m/%d')
+    # request.data = request.POST + request.FILES
+    # request.FIELS['photo_file'] -> MEDIA_ROOT
+    # path: photo.photo_file.path  MEDIA_ROOT/photo/~/~/~/~.jpg (절대 경로) - 경로에 저장
+    # url: photo.photo_file.url  MEDIA_URL/photo/~/~/~/~.jpg (상대 경로) - DB에 문자열로 저장
+
+
+class SpecificContent(models.Model):
+    specific_information = models.ForeignKey('Reservation', on_delete=models.CASCADE)
+    #여기 어떻게 할지 고민 
+
+
--- CultureServer/user/models.py
+++ CultureServer/user/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.core.mail import send_mail 
 
 # Create your models here.
+'''class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('사용자의 이메일이 없습니다.')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('관리자가 아닙니다.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('관리자가 아닙니다.')
+        return self._create_user(email, password, **extra_fields)'''
+
+class User(models.Model):
+    gender_choices = ((
+      ('남성', '남성'),
+      ('여성', '여성'),
+    ))
+    reservation = models.ForeignKey('Reservation', on_delete=models.CASCADE)
+    name = models.CharField('이름', max_length=30)
+    email = models.EmailField('이메일', unique=True)
+    contact = models.CharField('휴대폰 번호', max_length=15)
+    birth = models.DateField('생년월일', blank=True, null=True)
+    gender = models.CharField('성별', max_length=5, choices=gender_choices)
+
+    #objects = UserManager()
+
+    username_field = 'email'
+    required_fields = []
+
+    class Meta:
+      verbose_name = ('user')
+      verbose_name_plural = _('users')
+    
+    def __str__(self):
+      return self.name 
+    
+    def get_name(self):
+      return self.name
+    
+    def email_user(self, subject, message, from_email=None, **kwargs):
+      send_mail(subject, message, from_email, [self.email], **kwargs)

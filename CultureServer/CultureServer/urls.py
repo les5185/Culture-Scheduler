e26c0f078c90a69e588968ea9b794c07ce047d42
Tiffany Lee <tiffanylee3123@gmail.com>
2021-05-29 01:32:37 +0900

    add media urls

--- CultureServer/CultureServer/urls.py
+++ CultureServer/CultureServer/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_jwt.views import obtain_jwt_token
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +28,5 @@ urlpatterns = [
     path('reservations/', include('reservations.urls')),
     path('token-auth/', obtain_jwt_token)
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
